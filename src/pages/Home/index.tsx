e55f2c1afc3f0955cf62f9ed175a997f0a233cb6
nicholas lima <nichollas36@hotmail.com>
2023-04-04 20:34:33 -0300

    feat(home page): rendering states from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,27 @@ import logo from '../../assets/icons/logo.svg'
 import dogs from '../../assets/images/dogs.png'
 import search from '../../assets/icons/search.svg'
 import arrowDown from '../../assets/icons/chevron-bottom.svg'
+import { useForm } from 'react-hook-form'
+import { api } from '@/api/api'
+import { useEffect, useState } from 'react'
+
+type StateType = {
+  sigla: string
+  name: string
+}
 
 export function Home() {
-  function handleSearchPets() {
-    // TO DO
+  const { handleSubmit, register } = useForm()
+  const [states, setStates] = useState<StateType[]>([])
+
+  useEffect(() => {
+    api.get('/location/states').then((response) => {
+      setStates(response.data.states)
+    })
+  }, [])
+
+  function handleSearchPets(data: any) {
+    console.log(data)
   }
 
   function handleChangeState() {
@@ -32,20 +49,23 @@ export function Home() {
           <p className="title">
             Encontre o animal de estimação ideal para seu estilo de vida!
           </p>
-          <form>
-            <p>Busque um amigo:</p>
+
+          <form onSubmit={handleSubmit(handleSearchPets)}>
+            <p>Busque um amigo :</p>
             <div className="estados">
-              <select>
-                <option value="PE">PE</option>
-                <option value=""></option>
-                <option value=""></option>
+              <select {...register('state')}>
+                {states.map((state) => (
+                  <option key={state.sigla} value={state.sigla}>
+                    {state.sigla}
+                  </option>
+                ))}
               </select>
               <img src={arrowDown} alt="icone flexa para abrir o select" />
             </div>
 
             <div className="cidades">
-              <select id="">
-                <option value="">Recife</option>
+              <select {...register('city')}>
+                <option value="recife">Recife</option>
                 <option value=""></option>
                 <option value=""></option>
               </select>
